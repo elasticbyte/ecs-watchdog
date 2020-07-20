c1b73db61a9d07a88cc7dc296e29e4372d5e4b67
Justin <justin@commando.io>
2020-07-20 16:24:36 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecs-deploy-watchguard
+
+Post a Slack notifications to a channel when an AWS ECS task goes into STOPPED status.
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { IncomingWebhook } = require('@slack/webhook');
+
+if(! process.env.SLACK_WEBHOOK_URL) {
+    console.error('Missing required environment variable SLACK_WEBHOOK_URL');
+    process.exit(3);
+}
+
+const webhookUrl = process.env.SLACK_WEBHOOK_URL;
+const webhook = new IncomingWebhook(webhookUrl);
+
+exports.handler = async (event, context) => {
+    const account = event.account;
+    const region = event.region;
+    const service = event.detail.group;
+    const lastStatus = event.detail.lastStatus;
+    const stoppedReason = event.detail.stoppedReason;
+
+    if(lastStatus === "STOPPED") {
+        const message = `_[${account}]_ **${service}** in ${region} is **STOPPED**. Reason: ${stoppedReason}. Please investigate.`;
+        console.error(message);
+
+        (async () => {
+            await webhook.send({
+                text: message
+            });
+        })();
+    } else {
+        console.log(`[${account}] ${service} in ${region} is ${lastStatus}`);
+    }
+};
